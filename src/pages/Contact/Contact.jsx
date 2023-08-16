6befa2ebdaafa5626ea3af4d7c2e92f2c767dfa9
JuanBarberis <juanbarberis.97@gmail.com>
2023-08-16 11:49:47 -0300

    form listo

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './contact.css'
 
 function Contact() {
+  const [data, setData] = useState({
+    name: '',
+    mail: '',
+    message: '',
+  })
+  console.log(data)
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <>
-      <div id='contact'>Contact</div>
-      <h1> soy Contacto</h1>
+      <div id='contact' >
+        <h1 className='contact-title'>CONTACTO</h1>
+        <form className='form-contacto' action="https://formsubmit.co/6d4a06dc809795eb8671e9f32e82bd1e" method="POST"  >
+          <label className='label-form' for="name">Nombre</label>
+          <input className='input-form' type="text" id="name" name="name" value={data.name} onChange={handleChange} />
+          <label className='label-form' for="mail">Correo electrónico</label>
+          <input className='input-form' type="email" id="mail" name="mail" value={data.mail} onChange={handleChange} />
+          <label className='label-form' for="msg">Mensaje:</label>
+          <textarea className='area-form' id="msg" name="message" value={data.message} onChange={handleChange} />
+          <button className='button-form' type='submit' id='submit'> Enviar</button>
+
+          <input type='hidden' name='_next' value="http://localhost:3000/" />
+          <input type='hidden' name='_captcha' value="false" />
+        </form>
+      </div>
     </>
   )
 }
